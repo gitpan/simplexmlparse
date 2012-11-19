3e05990f6ba7dbdf5f7f4227fbaade5f9a4ff28d
Daniel Edward Graham <dgraham@firstteamsoft.com>
2012-11-19 14:58:00 +0000

    Import of DGRAHAM/simplexmlparse_v1.2 from CPAN.
    gitpan-cpan-distribution: simplexmlparse
    gitpan-cpan-version:      v1.2
    gitpan-cpan-path:         DGRAHAM/simpleXMLParse/simplexmlparse_v1.2.tar.gz
    gitpan-cpan-author:       DGRAHAM
    gitpan-cpan-maturity:     released

---
+++ sxp.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+#
+# PROGRAM: sxp.pl
+# DESCR:   This program uses my simpleXMLParse.pm module
+#          to take as input an XML file, parse style
+#          and prints to STDOUT a Perl HASH (Dumper)
+#          representing this parse.
+# v. 1.0, Daniel Edward Graham, 10/10/2012
+# LGPL 3.0
+# 
+# Usage: sxp.pl <inputfile> <parsestyle>
+#
+# <inputfile> ::= the xml file to parse
+# <parsestyle> ::= 1 or 2 (optional)
+#
+use simpleXMLParse;
+use Data::Dumper;
+
+my $fn = $ARGV[0];
+my $style = $ARGV[1];
+
+my $parse = new simpleXMLParse({input => $fn, style => $style});
+
+print Dumper($parse->data);
+
+
